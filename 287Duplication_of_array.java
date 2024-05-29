bb0c92733fe93aaa71ab372e0056adc9a0d7bee7
VishnuMK2006 <155707903+VishnuMK2006@users.noreply.github.com>
2024-05-29 08:08:27 -0700

    Update 287Duplication_of_array.java

--- 287Duplication_of_array.java
+++ 287Duplication_of_array.java
@@ -7,3 +7,17 @@ class Solution {
 
 }
 }
+//1ms --o(n) complexity but we use extra space of o(n) to achive efficient way
+/*class Solution {
+    public int findDuplicate(int[] nums) {
+byte b[]=new byte[nums.length];
+int n,i=-1;
+while(i<nums.length){
+    n=nums[++i];
+    if(b[n]==1)
+    return nums[i];
+b[n]=1;
+}
+return 0;
+}
+}*/
